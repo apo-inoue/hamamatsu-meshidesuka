52c925f449bfd7b7b8c9195472ca6dbee57c74f3
apo-inoue <takeshi.inoue@stransa.co.jp>
2021-02-21 05:18:29 +0900

    feature: 検索 BOX”

--- src/pages/restaurantList/index.tsx
+++ src/pages/restaurantList/index.tsx
@@ -1,23 +1,59 @@
-import { Box, CircularProgress, Paper, Typography } from '@material-ui/core';
+import {
+  Box,
+  CircularProgress,
+  Paper,
+  Typography,
+  InputBase,
+  IconButton,
+} from '@material-ui/core';
+import { Search } from '@material-ui/icons';
 import React, { Suspense } from 'react';
 import { ErrorBoundary } from 'react-error-boundary';
+import { makeStyles } from '@material-ui/core/styles';
 import { Layout } from '../../components/layout';
 import { ApiCall } from './ApiCall';
 import { ErrorMessage } from '../../components/ui/errorMessage/index';
 
-export const RestaurantList: React.VFC = () => (
-  <Layout>
-    <Paper variant="outlined">
-      <Box my={4} mx={3}>
-        <Typography variant="h6" paragraph>
-          レストラン一覧
-        </Typography>
-        <ErrorBoundary FallbackComponent={ErrorMessage}>
-          <Suspense fallback={<CircularProgress />}>
-            <ApiCall />
-          </Suspense>
-        </ErrorBoundary>
-      </Box>
-    </Paper>
-  </Layout>
-);
+const useStyles = makeStyles((theme) => ({
+  input: {
+    marginLeft: theme.spacing(9),
+    flex: 1,
+    maxWidth: 100,
+  },
+  iconButton: {
+    padding: 10,
+  },
+}));
+
+export const RestaurantList: React.VFC = () => {
+  const classes = useStyles();
+
+  return (
+    <Layout>
+      <Paper variant="outlined">
+        <Box my={4} mx={3}>
+          <Box display="flex" alignItems="center">
+            <Typography variant="h6">レストラン一覧</Typography>
+            <InputBase
+              className={classes.input}
+              placeholder="検索..."
+              inputProps={{ 'aria-label': 'search google maps' }}
+            />
+            <IconButton
+              type="submit"
+              className={classes.iconButton}
+              aria-label="search"
+            >
+              <Search />
+            </IconButton>
+          </Box>
+          <ErrorBoundary FallbackComponent={ErrorMessage}>
+            <Suspense fallback={<CircularProgress />}>
+              <ApiCall />
+            </Suspense>
+          </ErrorBoundary>
+        </Box>
+      </Paper>
+    </Layout>
+  );
+};
